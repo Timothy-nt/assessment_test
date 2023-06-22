f33ea0ca466aa330e7fb1bd2a3db0de9de75d6ab
Timothy-nt <93092972+Timothy-nt@users.noreply.github.com>
2023-06-22 12:53:03 +0300

    Create Java file: AverageCalculator.java

---
+++ Java file: AverageCalculator.java
@@ -0,0 +1,16 @@
+public class AverageCalculator {
+    public static double calculateAverage(int[] numbers) {
+        int total = 0;
+        for (int num : numbers) {
+            total += num;
+        }
+        double average = (double) total / numbers.length; // Perform floating-point division
+        return average;
+    }
+
+    public static void main(String[] args) {
+        int[] data = {5, 10, 15, 20, 25};
+        double result = calculateAverage(data);
+        System.out.println("The average is: " + result);
+    }
+}
